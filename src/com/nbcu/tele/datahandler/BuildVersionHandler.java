014001f93499d6da18084e36a9895c276ad10ddb
Alena Liamtsava <alena_liamtsava@epam.com>
2014-05-16 15:08:21 +0300

    add

---
+++ src/com/nbcu/tele/datahandler/BuildVersionHandler.java
@@ -0,0 +1,31 @@
+package com.nbcu.tele.datahandler;
+
+import java.io.IOException;
+
+public class BuildVersionHandler {
+	
+	private String currentBuild;
+	private String lastParsedBuildVersion;
+	private boolean isVersionChanged = false; 
+	
+	public BuildVersionHandler() {
+	}
+
+	public boolean compareVersions(String url, String fileName) throws IOException {
+		BuildVersionReader reader = new BuildVersionReader();
+		currentBuild = reader.readFromWebPage(url);
+//		currentBuild = "Build v2.0.5";
+		lastParsedBuildVersion = reader.readSavedBuildVerisonFromFile(fileName).trim();
+
+		if (!lastParsedBuildVersion.equalsIgnoreCase(currentBuild)){
+			isVersionChanged = true;
+			updateBuildVersionInFile(currentBuild, fileName);
+		}
+		return isVersionChanged;
+	}
+	
+	private void updateBuildVersionInFile(String currentBuild, String fileName){
+		BuildVersionUpdater writer = new BuildVersionUpdater();
+		writer.updateVersionInFile(currentBuild, fileName);
+	}
+}
